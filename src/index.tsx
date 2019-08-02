617ee7b8ae7a560d19101deb6e9773f86ed5ff02
yuzu <s.yuzu441@gmail.com>
2019-08-02 10:49:54 +0900

    Fix refactoring history area

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,12 @@ import * as React from 'react';
 import { render } from 'react-dom';
 // @ts-ignore
 import imags from './assets/img/*.png';
+import HistoryComponent from './history/'
+
 const App = () => (
   <div>
     <img src={imags['i_am_god']} alt="はいさん神ってる" />
-    <h2>history</h2>
-    <ul>
-      <li>201704: 世代代表になる</li>
-    </ul>
+    <HistoryComponent /> 
   </div>
 );
 render(<App />, document.getElementById('root'));
